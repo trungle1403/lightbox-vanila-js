2b71337ceb631d36f3da1fcab66464adbbb56df4
trungle1403 <64635586+trungle1403@users.noreply.github.com>
2021-08-05 01:45:05 +0700

    add function save in localstorage

--- api.js
+++ api.js
@@ -1,14 +1,15 @@
 
+
 const names = {
-    1: 'IU',
-    2: 'yuna',
-    3: 'yuqi',
-    4: 'hee jin',
-    5: 'mi yeon',
-    6: 'kha han',
-    7: 'phuong ngan',
-    8: 'thuy linh',
-    9: 'vu thi khanh huyen',
+    iu: 'IU',
+    yuna: 'yuna',
+    yuqi: 'yuqi',
+    heejin: 'hee jin',
+    miyeon: 'mi yeon',
+    khahan: 'kha han',
+    phuongngan: 'phuong ngan',
+    thuylinh: 'thuy linh',
+    khanhhuyen: 'vu thi khanh huyen',
 }
 
 const categorys = {
@@ -20,152 +21,119 @@ const categorys = {
     6: 'actor',
 
 }
+const imgArr = [
+    iu = [
+        'IU',
+        'https://i.pinimg.com/564x/d6/4a/86/d64a86c25d26a18cdfad916ddaa16e43.jpg',
+        'https://f18-zpg.zdn.vn/2736332357556461257/000e79d78bf87ca625e9.jpg',
+        'https://f41-zpg.zdn.vn/3166378426295995546/d36f7f8e8da17aff23b0.jpg',
+        'https://i.pinimg.com/564x/d6/22/0d/d6220d80f2ebf1655225186d1d4bc9d5.jpg',
+        'https://i.pinimg.com/564x/c7/94/0b/c7940bac0670687aed71495a9803c4a1.jpg'
+    ],
+    yuqi = [
+        'yuqi',
+        'https://i.pinimg.com/564x/4e/28/71/4e28715334e4202b8198c9ea8df8261f.jpg',
+        'https://i.pinimg.com/564x/1b/08/6c/1b086c4233b4621c2d62e5d1fdc79de3.jpg',
+        'https://i.pinimg.com/564x/58/dd/ab/58ddab98bd23d5b3d2ac7841afa8512e.jpg'
+
+    ],
+    heejin = [
+        'hee jin',
+        'https://i.pinimg.com/564x/b2/3c/ef/b23cefecccfe85909320714f10ce5ec8.jpg',
+        'https://i.pinimg.com/564x/55/c9/7b/55c97b3eda7b88fe39a548b461b0a151.jpg',
+        'https://i.pinimg.com/564x/a2/bd/5a/a2bd5acd2d19b14577efc58df47c7cf0.jpg',
+
+    ],
+    miyeon = [
+        'mi yeon',
+        'https://i.pinimg.com/564x/db/8e/ec/db8eec1b3a5c53f065d29752cd10168c.jpg',
+    ],
+    yuna = [
+        'yuna',
+        'https://i.pinimg.com/564x/6f/70/77/6f7077fb96b0a481bbc704efd307f865.jpg',
+        'https://i.pinimg.com/564x/00/83/d4/0083d40d87a455ede9954bd262020a5a.jpg',
+        'https://i.pinimg.com/564x/20/b4/c4/20b4c4f11ad3b22ca5ba5d9f1f6136aa.jpg',
+        'https://i.pinimg.com/564x/a6/1e/22/a61e22ac7895944c36aa0a2bb27b2489.jpg',
+        'https://scontent.fsgn5-6.fna.fbcdn.net/v/t1.6435-9/119663234_330101524721023_2273985977509110839_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=To2yO-WxgpwAX8YpVMg&_nc_ht=scontent.fsgn5-6.fna&oh=95a1d1cc579dcb88d19fdbb06acc022a&oe=612B5A3F',
+        'https://i.pinimg.com/564x/c1/58/fc/c158fcb48928fd7f6e1dfac2b09a3c67.jpg'
+
+
+    ],
+    khahan = [
+        'kha han',
+        'https://i.pinimg.com/564x/6f/70/77/6f7077fb96b0a481bbc704efd307f865.jpg',
+    ],
+    thuylinh = [
+        'thuy linh',
+        'https://scontent.fsgn8-1.fna.fbcdn.net/v/t1.6435-9/159844830_1424399811233617_4679297703946795463_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=Z9m_pLoIJ_UAX8FrldD&_nc_ht=scontent.fsgn8-1.fna&oh=6469f4ca85ff81ad7c6e3dd0c2f8f0b0&oe=612C5455',
+        'https://scontent.fsgn8-1.fna.fbcdn.net/v/t1.6435-9/124848785_1335960793410853_193957594087227736_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=M-r3nfAPpKcAX9xItYd&_nc_ht=scontent.fsgn8-1.fna&oh=c2a4a54c2e98bdd1422ca5971cb063e7&oe=612CC13E'
+    ],
+    khanhhuyen = [
+        'vu thi khanh huyen',
+        'https://scontent.fsgn3-1.fna.fbcdn.net/v/t1.6435-9/228132283_351471873332526_2083963259859167035_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=825194&_nc_ohc=w3MkWV1jMEEAX8CgNoY&_nc_ht=scontent.fsgn3-1.fna&oh=f9179fc27bf124ffe836541a1e72b138&oe=612D7C69',
+
+    ],
+    other = [
+
+    ]
+]
 
 const dataImg = [
     {
         id: 1,
-        name: names[1],
+        name: names['iu'],
         category: [categorys[1], categorys[2], categorys[6]],
-        src: 'https://i.pinimg.com/564x/d6/4a/86/d64a86c25d26a18cdfad916ddaa16e43.jpg'
+        src: imgArr[0]
     },
     {
         id: 2,
-        name: names[1],
-        category: [categorys[1], categorys[2], categorys[6]],
-        src: 'https://f18-zpg.zdn.vn/2736332357556461257/000e79d78bf87ca625e9.jpg'
+        name: names['yuqi'],
+        category:[categorys[1], categorys[2]],
+        src: imgArr[1]
     },
     {
         id: 3,
-        name: names[1],
-        category:[categorys[1], categorys[2], categorys[6]],
-        src: 'https://f41-zpg.zdn.vn/3166378426295995546/d36f7f8e8da17aff23b0.jpg'
+        name: names['heejin'],
+        category:[categorys[1], categorys[2]],
+        src: imgArr[2]
     },
+    
     {
         id: 4,
-        name: names[1],
-        category: [categorys[1], categorys[2], categorys[6]],
-        src: 'https://i.pinimg.com/564x/d6/22/0d/d6220d80f2ebf1655225186d1d4bc9d5.jpg'
+        name: names['miyeon'],
+        category: [categorys[1], categorys[2]],
+        src: imgArr[3]
     },
     {
         id: 5,
-        name: names[4],
+        name: names['yuna'],
         category:[categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/b2/3c/ef/b23cefecccfe85909320714f10ce5ec8.jpg'
+        src: imgArr[4]
     },
     {
         id: 6,
-        name: names[4],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/55/c9/7b/55c97b3eda7b88fe39a548b461b0a151.jpg'
+        name: names['khahan'],
+        category: [categorys[1]],
+        src:imgArr[5]
     },
     {
         id: 7,
-        name: names[5],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/db/8e/ec/db8eec1b3a5c53f065d29752cd10168c.jpg'
+        name: names['thuylinh'],
+        category: [categorys[1]],
+        src: imgArr[6]
     },
     {
         id: 8,
-        name: names[2],
-        category:[categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/6f/70/77/6f7077fb96b0a481bbc704efd307f865.jpg'
-    },
-    {
-        id: 9,
-        name: 'Doctor strange',
-        category:[categorys[4],categorys[5]],
-        src: 'https://i.pinimg.com/564x/fc/fb/b8/fcfbb8e32f779d3e39e33825b87c8175.jpg'
+        name: names['khanhhuyen'],
+        category: [categorys[1]],
+        src: imgArr[7]
     },
     {
         id: 10,
-        name: 'Kha han',
+        name: 'girl from nowhere',
         category: [categorys[1]],
-        src: 'https://scontent-xsp1-3.xx.fbcdn.net/v/t1.6435-9/166241985_2855738538025699_3341946107539893583_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=PyCIFBu1xLgAX-FDX2Y&_nc_ht=scontent-xsp1-3.xx&oh=f54b9712e93388ef7adbf489a89e9ff9&oe=612C8E01'
+        src: imgArr[8]
     },
-    {
-        id: 11,
-        name: names[2],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/00/83/d4/0083d40d87a455ede9954bd262020a5a.jpg'
-    },
-    {
-        id: 12,
-        name: names[1],
-        category:[categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/c7/94/0b/c7940bac0670687aed71495a9803c4a1.jpg'
-    },
-    {
-        id: 13,
-        name: names[2],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/20/b4/c4/20b4c4f11ad3b22ca5ba5d9f1f6136aa.jpg'
-    },
-    {
-        id: 14,
-        name: names[3],
-        category:[categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/4e/28/71/4e28715334e4202b8198c9ea8df8261f.jpg'
-    },
-    {
-        id: 15,
-        name: names[3],
-        category:[categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/1b/08/6c/1b086c4233b4621c2d62e5d1fdc79de3.jpg'
-    },
-    {
-        id: 16,
-        name: names[2],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/a6/1e/22/a61e22ac7895944c36aa0a2bb27b2489.jpg'
-    },
-    {
-        id: 17,
-        name: names[4],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/a2/bd/5a/a2bd5acd2d19b14577efc58df47c7cf0.jpg'
-    },
-    {
-        id: 18,
-        name: 'phuong ngan',
-        category: [categorys[1]],
-        src: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.6435-9/102730183_1361084950759535_3304245744427977181_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=N2shy5v6ybAAX-0sRso&tn=iwpLnBEHADVMhIWN&_nc_ht=scontent-hkt1-1.xx&oh=640142293617fd27f60900b54679f722&oe=612B3A5A'
-    },
-    {
-        id: 19,
-        name: names[2],
-        category: [categorys[1]],
-        src: 'https://scontent.fsgn5-6.fna.fbcdn.net/v/t1.6435-9/119663234_330101524721023_2273985977509110839_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=To2yO-WxgpwAX8YpVMg&_nc_ht=scontent.fsgn5-6.fna&oh=95a1d1cc579dcb88d19fdbb06acc022a&oe=612B5A3F'
-    },
-    {
-        id: 20,
-        name: names[8],
-        category: [categorys[1]],
-        src: 'https://scontent.fsgn8-1.fna.fbcdn.net/v/t1.6435-9/159844830_1424399811233617_4679297703946795463_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=Z9m_pLoIJ_UAX8FrldD&_nc_ht=scontent.fsgn8-1.fna&oh=6469f4ca85ff81ad7c6e3dd0c2f8f0b0&oe=612C5455'
-    },
-    {
-        id: 21,
-        name: names[9],
-        category: [categorys[1]],
-        src: 'https://scontent.fsgn3-1.fna.fbcdn.net/v/t1.6435-9/228132283_351471873332526_2083963259859167035_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=825194&_nc_ohc=w3MkWV1jMEEAX8CgNoY&_nc_ht=scontent.fsgn3-1.fna&oh=f9179fc27bf124ffe836541a1e72b138&oe=612D7C69'
-    },
-    {
-        id: 22,
-        name: names[8],
-        category: [categorys[1]],
-        src: 'https://scontent.fsgn8-1.fna.fbcdn.net/v/t1.6435-9/124848785_1335960793410853_193957594087227736_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=M-r3nfAPpKcAX9xItYd&_nc_ht=scontent.fsgn8-1.fna&oh=c2a4a54c2e98bdd1422ca5971cb063e7&oe=612CC13E'
-    },
-    {
-        id: 23,
-        name: names[3],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/58/dd/ab/58ddab98bd23d5b3d2ac7841afa8512e.jpg'
-    },
-    {
-        id: 24,
-        name:names[2],
-        category: [categorys[1], categorys[2]],
-        src: 'https://i.pinimg.com/564x/c1/58/fc/c158fcb48928fd7f6e1dfac2b09a3c67.jpg'
-    },
+    
 ]
-
 const colors = ['blue','green','yellow','orange','pink']
--- main.js
+++ main.js
@@ -60,61 +60,85 @@ function createImage(data){
         // lay so trong mang ngau nhien
         //[2,3,4,1,5,23,12,5...]
         let num = newArr[i]
-
-        let imgEle = document.createElement('img')
-        imgEle.setAttribute('src',data[num].src)
-        imgEle.classList.add('image')
         
+        data[num].src.map(srcItem =>{
+            if(srcItem != data[num].name){
+                let imgEle = document.createElement('img')
+                imgEle.setAttribute('src',srcItem)
+                imgEle.classList.add('image')
 
-        //images > content-image > desc > image + name + {category > item}
-        //create div parent
-        let divParent = document.createElement('div');
-        divParent.classList.add('content-image');
-        divParent.setAttribute('data', data[num].name)
+                //images > content-image > desc > image + name + {category > item}
+                //create div parent
+                let divParent = document.createElement('div');
+                divParent.classList.add('content-image');
+                divParent.setAttribute('data', data[num].name)
 
-        //create div desc 
-        let divDesc = document.createElement('div');
-        divDesc.classList.add('desc-image');
+                //create div desc 
+                let divDesc = document.createElement('div');
+                divDesc.classList.add('desc-image');
 
-        //create div category 
-        let divCategory = document.createElement('div');
-        divCategory.classList.add('category-image');
+                //create div category 
+                let divCategory = document.createElement('div');
+                divCategory.classList.add('category-image');
 
-        data[num].category.forEach(item =>{
-            //span category-item
-            let spanCategoryItem = document.createElement('span');
-            spanCategoryItem.classList.add('category-item');
-            spanCategoryItem.classList.add(randomColor());
-            spanCategoryItem.textContent = item;
+                data[num].category.forEach(item =>{
+                    //span category-item
+                    let spanCategoryItem = document.createElement('span');
+                    spanCategoryItem.classList.add('category-item');
+                    spanCategoryItem.classList.add(randomColor());
+                    spanCategoryItem.textContent = item;
 
-            divCategory.appendChild(spanCategoryItem);
+                    divCategory.appendChild(spanCategoryItem);
+                })
+                
+
+                //create div name 
+                let divName = document.createElement('div');
+                divName.classList.add('name-image');
+                divName.textContent = data[num].name;
+
+                //add to div desc-image
+                divDesc.appendChild(divCategory)
+                divDesc.appendChild(divName)
+                
+                // add to div parent
+                divParent.appendChild(imgEle)
+                divParent.appendChild(divDesc)
+                divParent.addEventListener('click',showLightbox)
+            
+                imageContainer.appendChild(divParent)
+            }
         })
         
-
-        //create div name 
-        let divName = document.createElement('div');
-        divName.classList.add('name-image');
-        divName.textContent = data[num].name;
-
-        //add to div desc-image
-        divDesc.appendChild(divCategory)
-        divDesc.appendChild(divName)
-        
-        // add to div parent
-        divParent.appendChild(imgEle)
-        divParent.appendChild(divDesc)
-        divParent.addEventListener('click',showLightbox)
-    
-        imageContainer.appendChild(divParent)
     }
     
 }
-createImage(dataImg)
-//create event with html
-// images.forEach(image => image.addEventListener('click',showLightbox));
-// for(let i = 0; i < images.length; i++) {
-//     images[i].addEventListener('click',showLightbox)
-// }
+// createImage(dataImg)
+addLocalStorage()
+// ======================== localStorage ======================================
+function allStorage() {
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]) );
+    }
+    return values;
+}
+function addLocalStorage() {
+    // clearData()
+    if (typeof(Storage) !== 'undefined') {
+        let arrStorage = allStorage();
+        arrStorage.map(item => {
+            imgArr[8].push(item)
+        })
+    }
+    createImage(dataImg)
+
+}
+
+
 txtSearch.addEventListener('keyup',(e) => {
     let contentImage = document.querySelectorAll('.images .content-image');
     contentImage.forEach(item => {
@@ -168,11 +192,11 @@ document.body.addEventListener('click', (e) =>{
         lightImage.setAttribute('src',newSrc)
     }
     
-    if (e.target.matches('.btn-close')){
+    if (e.target.matches('.lightbox .btn-close')){
         //remove lightbox out of dom
         document.body.removeChild(e.target.parentNode);
     }
-    if (e.target.matches('.btn-close span')){
+    if (e.target.matches('.lightbox .btn-close span')){
         //remove lightbox out of dom
         document.body.removeChild(e.target.parentNode.parentNode);
     }
@@ -184,3 +208,48 @@ window.addEventListener('scroll',function(){
     var header = document.querySelector('.header');
     header.classList.toggle('sticky',window.scrollY>0);
 })
+// scroll bar
+// let progress = document.getRootNode(scrollbars);
+let progress = document.querySelector('.progressBar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight)*100;
+    progress.style.height = progressHeight + "%";
+}
+// scroll bar end
+
+
+
+// ========================= modal =======================================
+
+const btnShowModal = document.querySelector('.btn-show-modal')
+const modalAdd = document.querySelector('.modal-add')
+const btnAddModal = document.querySelector('.modal-add .btn-add')
+const btnCloseModal = document.querySelector('.modal-add .btn-close')
+const btnCancelModal = document.querySelector('.modal-add .btn-cancel')
+
+
+btnShowModal.addEventListener('click', () => {
+    modalAdd.classList.toggle('active')
+})
+btnCloseModal.addEventListener('click', () => {
+    modalAdd.classList.toggle('active')
+})
+btnCancelModal.addEventListener('click', () => {
+    modalAdd.classList.toggle('active')
+})
+
+
+btnAddModal.addEventListener('click', () => {
+    const urlImg = document.querySelector('.modal-add .url-image')
+    let valueImg = urlImg.value
+    if(valueImg != '') {
+        localStorage.setItem(valueImg,valueImg)
+    }
+    modalAdd.classList.toggle('active')
+    urlImg.value = ''
+    window.location.reload()
+})
+
+// localStorage.setItem('test', 'https://i.pinimg.com/564x/d6/4a/86/d64a86c25d26a18cdfad916ddaa16e43.jpg');
+// localStorage.clear()
